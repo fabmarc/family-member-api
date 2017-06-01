ca34ecd9439a514dbab6b159e9fda8862724b299
fabmarc <fabio.mfp@gmail.com>
2017-06-01 20:55:49 +0200

    Add update method

--- src/main/java/com/studies/controller/MemberController.java
+++ src/main/java/com/studies/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,5 +72,22 @@ public class MemberController {
 		MemberBean memberBean = MemberMapper.toBean(theMember);
 
 		return memberBean;
+	}
+
+	
+	@PutMapping(path = "/member/{idMember}")
+	public ResponseEntity<MemberBean> updateMember(@PathVariable Integer idMember, @RequestBody MemberBean newMember) {
+
+		Member theMember = memberReposity.findOne(idMember);
+		if (theMember == null) {
+			return new ResponseEntity<MemberBean>(HttpStatus.NOT_FOUND);
+		}
+		Member member = MemberMapper.toEntity(newMember);
+		member.setId(idMember);
+		
+		theMember = memberReposity.save(member);
+		
+		MemberBean memberBean = MemberMapper.toBean(theMember);
+		return new ResponseEntity<MemberBean>(memberBean, HttpStatus.OK);
 	}
 }
